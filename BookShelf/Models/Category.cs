2269c8a78af25cb7d403532acee0b0e34250db8b
bgklein7 <bgklein@outlook.com>
2018-03-06 15:28:37 -0500

    first commit

---
+++ BookShelf/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookShelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        //Navigation Property
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
